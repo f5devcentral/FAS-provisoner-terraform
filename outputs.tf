cff7143e9c870eb454e6d7fe40c18dbb260904c9
Sanjay Shitole <scshitole>
2020-01-25 16:51:20 -0800

    added f5

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,18 @@ output "ec2_ip" {
    value = "${aws_instance.fas.public_ip}"
  }
 
+ output "F5_IP" {
+  value = aws_eip.f5.public_ip
+}
+
+output "F5_Password" {
+  value = random_string.password.result
+}
+
+output "f5_ui" {
+  value = "https://${aws_eip.f5.public_ip}:8443"
+}
+
 
 output "To_SSH_into_fas_ubuntu" {
   value =  "ssh -i ${aws_key_pair.demo.key_name}.pem ubuntu@${aws_instance.fas.public_ip}"
